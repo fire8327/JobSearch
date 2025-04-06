a5aa16302f419f2af9c3bc7ffc131b1391b15c71
fire8327 <fire83274@gmail.com>
2025-04-06 20:10:16 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -30,22 +30,22 @@
             <div class="flex flex-col gap-6 items-center text-center">
                 <img src="/images/advantages/1.png" alt="" class="w-full lg:w-1/2">
                 <p class="text-2xl font-mono font-semibold">Комфорт и баланс</p>
-                <p>Без привязки к офису и строгому расписанию — у нас удалёнка и удобный график. У вас будет время на обучение, развитие и личные дела</p>
+                <p class="max-w-lg">Без привязки к офису и строгому расписанию — у нас удалёнка и удобный график. У вас будет время на обучение, развитие и личные дела</p>
             </div>
             <div class="flex flex-col gap-6 items-center text-center">
                 <img src="/images/advantages/2.png" alt="" class="w-full lg:w-1/2">
                 <p class="text-2xl font-mono font-semibold">Гарантии и бонусы</p>
-                <p>Трудоустройство официальное, с отпусками, больничными, выплатами без задержек. Скидки от партнёров и на продукты Яндекса</p>
+                <p class="max-w-lg">Трудоустройство официальное, с отпусками, больничными, выплатами без задержек. Скидки от партнёров и на продукты Яндекса</p>
             </div>
             <div class="flex flex-col gap-6 items-center text-center">
                 <img src="/images/advantages/3.png" alt="" class="w-full lg:w-1/2">
                 <p class="text-2xl font-mono font-semibold">Забота о сотрудниках</p>
-                <p>Организуем чаты, внутреннюю помощь и поддержку, тематические активности — чтобы на удалёнке расстояние между людьми не ощущалось</p>
+                <p class="max-w-lg">Организуем чаты, внутреннюю помощь и поддержку, тематические активности — чтобы на удалёнке расстояние между людьми не ощущалось</p>
             </div>
             <div class="flex flex-col gap-6 items-center text-center">
                 <img src="/images/advantages/4.png" alt="" class="w-full lg:w-1/2">
                 <p class="text-2xl font-mono font-semibold">Работа со смыслом</p>
-                <p>Неважно, какая у вас специальность: каждый сотрудник Яндекс Крауда помогает улучшать сервисы Яндекса</p>
+                <p class="max-w-lg">Неважно, какая у вас специальность: каждый сотрудник Яндекс Крауда помогает улучшать сервисы Яндекса</p>
             </div>
         </div>
     </div>
