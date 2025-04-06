bc310ae8a9fede7a25b72725e7b2af94962e0775
fire8327 <fire83274@gmail.com>
2025-04-06 20:09:28 +0300

    create 2 block on main

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,53 @@
 <template>
-    <div>
-        123
+    <div class="flex flex-col gap-6">
+        <p class="mainHeading">Как начать работать</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div class="flex flex-col gap-6 rounded-xl bg-gray-200 p-4 items-center text-center">
+                <p class="px-8 py-1.5 text-3xl font-mono bg-indigo-500 text-white rounded-lg">1</p>
+                <p class="text-2xl font-mono font-semibold">Отклик</p>
+                <p>Откликнитесь на вакансию</p>
+            </div>
+            <div class="flex flex-col gap-6 rounded-xl bg-gray-200 p-4 items-center text-center">
+                <p class="px-8 py-1.5 text-3xl font-mono bg-indigo-500 text-white rounded-lg">2</p>
+                <p class="text-2xl font-mono font-semibold">Отбор</p>
+                <p>Пройдите интервью и/или тестовое задание</p>
+            </div>
+            <div class="flex flex-col gap-6 rounded-xl bg-gray-200 p-4 items-center text-center">
+                <p class="px-8 py-1.5 text-3xl font-mono bg-indigo-500 text-white rounded-lg">3</p>
+                <p class="text-2xl font-mono font-semibold">Оформление</p>
+                <p>Подпишите договор и другие документы</p>
+            </div>
+            <div class="flex flex-col gap-6 rounded-xl bg-gray-200 p-4 items-center text-center">
+                <p class="px-8 py-1.5 text-3xl font-mono bg-indigo-500 text-white rounded-lg">4</p>
+                <p class="text-2xl font-mono font-semibold">Работа</p>
+                <p>Пройдите обучение и начинайте работу</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="mainHeading">Почему у нас классно</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div class="flex flex-col gap-6 items-center text-center">
+                <img src="/images/advantages/1.png" alt="" class="w-full lg:w-1/2">
+                <p class="text-2xl font-mono font-semibold">Комфорт и баланс</p>
+                <p>Без привязки к офису и строгому расписанию — у нас удалёнка и удобный график. У вас будет время на обучение, развитие и личные дела</p>
+            </div>
+            <div class="flex flex-col gap-6 items-center text-center">
+                <img src="/images/advantages/2.png" alt="" class="w-full lg:w-1/2">
+                <p class="text-2xl font-mono font-semibold">Гарантии и бонусы</p>
+                <p>Трудоустройство официальное, с отпусками, больничными, выплатами без задержек. Скидки от партнёров и на продукты Яндекса</p>
+            </div>
+            <div class="flex flex-col gap-6 items-center text-center">
+                <img src="/images/advantages/3.png" alt="" class="w-full lg:w-1/2">
+                <p class="text-2xl font-mono font-semibold">Забота о сотрудниках</p>
+                <p>Организуем чаты, внутреннюю помощь и поддержку, тематические активности — чтобы на удалёнке расстояние между людьми не ощущалось</p>
+            </div>
+            <div class="flex flex-col gap-6 items-center text-center">
+                <img src="/images/advantages/4.png" alt="" class="w-full lg:w-1/2">
+                <p class="text-2xl font-mono font-semibold">Работа со смыслом</p>
+                <p>Неважно, какая у вас специальность: каждый сотрудник Яндекс Крауда помогает улучшать сервисы Яндекса</p>
+            </div>
+        </div>
     </div>
 </template>
 
