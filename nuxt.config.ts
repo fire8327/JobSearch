945c043a007c3e0ed878f8ea0effafeb2294cdbb
fire8327 <fire83274@gmail.com>
2025-04-07 05:06:54 +0300

    add pinia, formkit, supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@nuxtjs/supabase',
+    '@formkit/nuxt'
+  ],
+  postcss: {
+    plugins: {
+      cssnano: false 
+    }
+  },
+
+  supabase: {
+    redirect: false
+  }
 })
